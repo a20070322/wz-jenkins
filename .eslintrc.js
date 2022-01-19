103fd6f288368638cbb76f3dbaf620d420726b9c
ZhaoZhongYang <zhaozhongyang@idso.net.cn>
2022-01-19 15:58:12 +0800

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'plugin:@typescript-eslint/recommended'
+    ],
+    parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        createDefaultProgram: true,
+    },
+    rules: {
+
+    }
+}
---
+++ rollup.config.js
@@ -0,0 +1,42 @@
+/** 将json转换为ES6模块 */
+const json = require("@rollup/plugin-json");
+
+/** rollup解析及编译TS插件 */
+const typescript = require("@rollup/plugin-typescript");
+
+/** 解析代码中依赖的node_modules */
+const resolve = require("@rollup/plugin-node-resolve");
+
+/** 将 CommonJS 模块转换为 ES6 的 Rollup 插件 */
+const commonjs = require("@rollup/plugin-commonjs");
+
+/** rollup文件夹清除插件 */
+const { cleandir } = require("rollup-plugin-cleandir");
+
+module.exports = {
+  /** 打包入口文件 */
+  input: ["./src/index.ts", "./src/cli.ts"],
+  /** 输出配置 */
+  output: {
+    /** 输出目录 */
+    dir: "./lib",
+    /** 输出文件为 CommonJS格式 */
+    format: "cjs",
+  },
+  plugins: [
+    /** 配置插件 - 每次打包清除目标文件 */
+    cleandir("./lib"),
+    /** 配置插件 - 将json转换为ES6模块 */
+    json(),
+    /** 配置插件 - 将json转换为ES6模块 */
+    typescript({ module: "CommonJS", }),
+    resolve.default({
+      extensions: [".js", ".ts", ".json"],
+      modulesOnly: true,
+      preferredBuiltins: false,
+    }),
+    commonjs({ extensions: [".js", ".ts", ".json"] }),
+  ],
+  /** 排除打包的模块 */
+  external: ["prompts", "axios", "chalk", "ora"],
+};
