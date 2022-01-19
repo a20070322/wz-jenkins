b56602ce8f43bb52aca21a51ec5344c239974d98
ZhaoZhongYang <zhaozhongyang@idso.net.cn>
2022-01-19 18:14:44 +0800

    add cac desc

--- src/cli.ts
+++ src/cli.ts
@@ -3,8 +3,10 @@ import { name, version } from "../package.json";
 
 const cli = cac(name);
 
+/** cli命令数组 */
 cli.commands = [
-  cli.command("", "执行jenkins脚本", {}).action(() => {
+  /** 命令name , 命令描述 , 命令配置 */
+  cli.command("", "执行jenkins脚本").action(() => {
     console.log("hello cli");
   }),
 ];
